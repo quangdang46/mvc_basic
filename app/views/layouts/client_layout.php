1c5dc9d6d7bad8e404316ef1bb1abac29f5e5f4c
quangdang46 <tranquangdang21@gmail.com>
2022-10-05 22:13:44 +0700

    master layout

--- app/views/layouts/client_layout.php
+++ app/views/layouts/client_layout.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo _WEB_ROOT ?>/public/clients/assets/css/app.css">
   <title>Document</title>
 </head>
 
 <body>
   <?php $this->render("blocks/header"); ?>
-  <p>Day la noi dung</p>
+  <?php $this->render($content); ?>
   <?php $this->render("blocks/footer"); ?>
+
+  <script src="<?php echo _WEB_ROOT ?>/public/clients/assets/js/main.js
+  "></script>
 </body>
 
 </html>
--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,23 @@
 
 <?php
 define("_DIR_ROOT", __DIR__);
+
+// xu li http root
+$web_root = "http://" . $_SERVER['HTTP_HOST'];
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") {
+  $web_root = "https://" . $_SERVER['HTTP_HOST'];
+}
+
+//lay thu muc chua code
+$root = str_replace("\\", "/", _DIR_ROOT);
+
+$folder = str_replace(strtolower($_SERVER['DOCUMENT_ROOT']), "", strtolower($root));
+$web_root = $web_root . $folder;
+// echo $web_root;
+define('_WEB_ROOT', $web_root);
+
+
+
 require_once "./configs/routers.php";
 require_once "core/Controller.php";
 require_once "./app/App.php";
